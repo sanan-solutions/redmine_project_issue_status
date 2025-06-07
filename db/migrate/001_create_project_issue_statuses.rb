7ae9f0aba886ed2be6b1acc1b4285e1b84db1c87
datnguyen <dat@sanancorp.com>
2025-06-07 14:36:09 +0700

    fix migration

--- db/migrate/001_create_project_issue_statuses.rb
+++ db/migrate/001_create_project_issue_statuses.rb
@@ -1,10 +1,13 @@
 class CreateProjectIssueStatuses < ActiveRecord::Migration[6.1]
   def change
     create_table :project_issue_statuses do |t|
-      t.references :project, null: false, foreign_key: true
-      t.references :issue_status, null: false
+      t.integer :project_id, null: false
+      t.integer :issue_status_id, null: false
       t.timestamps
     end
+
     add_index :project_issue_statuses, [:project_id, :issue_status_id], unique: true, name: 'index_project_status_unique'
+    add_foreign_key :project_issue_statuses, :projects, column: :project_id
+    add_foreign_key :project_issue_statuses, :issue_statuses, column: :issue_status_id
   end
 end
